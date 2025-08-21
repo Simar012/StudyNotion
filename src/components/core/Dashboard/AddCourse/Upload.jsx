877b1c6c750a658dab509e016e3c2f56a38fe8e3
Simar <ssimarsingh7@gmail.com>
2025-08-21 16:04:11 +0530

    fix: warnings

--- src/components/core/Dashboard/AddCourse/Upload.jsx
+++ src/components/core/Dashboard/AddCourse/Upload.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import { useDropzone } from "react-dropzone"
 import { FiUploadCloud } from "react-icons/fi"
-import { useSelector } from "react-redux"
 import { Player } from "video-react"
 import "video-react/dist/video-react.css"
 
