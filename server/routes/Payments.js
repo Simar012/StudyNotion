2253e19fd5f3e0b708634069860fcaedb2392ce4
Simar <ssimarsingh7@gmail.com>
2025-08-21 14:59:12 +0530

    Fix: Updated auth to Auth in Payments.js

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controller/Payments")
-const { auth, isInstructor, isStudent, isAdmin } = require("../middleware/auth")
+const { auth, isInstructor, isStudent, isAdmin } = require("../middleware/Auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment",auth, isStudent, verifyPayment)
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
